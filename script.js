0cb71cd515bdf9da6013ec3a829489a39267d986
puneet00164 <puneetv1988@gmail.com>
2024-04-04 12:23:22 -0400

    captcha

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
-function UpdateLeadDate()
+let captchaflag = false; 
+function UpdateLeadDate(event)
 {
+  if(captchaflag)
+  {   
    let inputdate1 = document.querySelector(".inputdate");
    let outputdate1 = document.querySelector(".outputdate");
 
@@ -8,4 +11,28 @@ function UpdateLeadDate()
 
    console.log('output date is ' + outputdate1.value);
    console.log('input date is '+ inputdate1.value); 
+}
+else{
+    alert(Please select captcha);
+    event.preventDefault(); 
+}
+}
+
+function timestamp() { 
+var response = document.getElementById("g-recaptcha-response"); 
+if (response == null || response.value.trim() == "") 
+{
+    var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);
+    
+    elems["ts"] = JSON.stringify(new Date().getTime());
+    
+    document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); 
+
+} 
+
+} setInterval(timestamp, 500); 
+
+function captchacheck()
+{
+    captchaflag = true; 
 }
